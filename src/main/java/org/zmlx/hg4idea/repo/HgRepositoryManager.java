30e595e84e04114ae8ce9c4f28aab8622bd3f17b
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-31 14:27:06 +0400

    update compile to last dvcs api

--- src/main/java/org/zmlx/hg4idea/repo/HgRepositoryManager.java
+++ src/main/java/org/zmlx/hg4idea/repo/HgRepositoryManager.java
@@ -4,7 +4,6 @@ import consulo.annotation.component.ComponentScope;
 import consulo.annotation.component.ServiceAPI;
 import consulo.annotation.component.ServiceImpl;
 import consulo.project.Project;
-import consulo.util.lang.ObjectUtil;
 import consulo.versionControlSystem.distributed.branch.DvcsSyncSettings;
 import consulo.versionControlSystem.distributed.repository.AbstractRepositoryManager;
 import consulo.versionControlSystem.distributed.repository.VcsRepositoryManager;
@@ -14,7 +13,6 @@ import jakarta.inject.Singleton;
 import org.zmlx.hg4idea.HgProjectSettings;
 import org.zmlx.hg4idea.HgVcs;
 import org.zmlx.hg4idea.branch.HgMultiRootBranchConfig;
-import org.zmlx.hg4idea.util.HgUtil;
 
 import java.util.List;
 
@@ -22,27 +20,25 @@ import java.util.List;
 @ServiceAPI(ComponentScope.PROJECT)
 @ServiceImpl
 public class HgRepositoryManager extends AbstractRepositoryManager<HgRepository> {
-  public static HgRepositoryManager getInstance(@Nonnull Project project) {
-    return project.getInstance(HgRepositoryManager.class);
-  }
+    public static HgRepositoryManager getInstance(@Nonnull Project project) {
+        return project.getInstance(HgRepositoryManager.class);
+    }
 
-  private final HgProjectSettings mySettings;
+    @Inject
+    public HgRepositoryManager(@Nonnull Project project,
+                               @Nonnull VcsRepositoryManager vcsRepositoryManager) {
+        super(project, vcsRepositoryManager, HgVcs.getKey());
+    }
 
-  @Inject
-  public HgRepositoryManager(@Nonnull Project project,
-                             @Nonnull VcsRepositoryManager vcsRepositoryManager) {
-    super(vcsRepositoryManager, HgVcs.getInstance(project), HgUtil.DOT_HG);
-    mySettings = ObjectUtil.assertNotNull(HgVcs.getInstance(project)).getProjectSettings();
-  }
+    @Override
+    public boolean isSyncEnabled() {
+        HgProjectSettings projectSettings = ((HgVcs) getVcs()).getProjectSettings();
+        return projectSettings.getSyncSetting() == DvcsSyncSettings.Value.SYNC && !new HgMultiRootBranchConfig(getRepositories()).diverged();
+    }
 
-  @Override
-  public boolean isSyncEnabled() {
-    return mySettings.getSyncSetting() == DvcsSyncSettings.Value.SYNC && !new HgMultiRootBranchConfig(getRepositories()).diverged();
-  }
-
-  @Nonnull
-  @Override
-  public List<HgRepository> getRepositories() {
-    return getRepositories(HgRepository.class);
-  }
+    @Nonnull
+    @Override
+    public List<HgRepository> getRepositories() {
+        return getRepositories(HgRepository.class);
+    }
 }
